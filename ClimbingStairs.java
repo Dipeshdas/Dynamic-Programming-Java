15556821b4cd920b412dd8556b5a4f81f097c16f
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2022-12-20 19:33:32 +0530

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,17 +1,27 @@
+//Memoization not possible here .
 class Solution {
     
       int dp[]=new int[46];
 
+    //Memoization
+//     int f(int n,int dp[]){
+//         if(n==1) return 1;
+//         if(n==0) return 1;
+//         if(dp[n]!=-1) return dp[n];
+//         int left=climbStairs(n-1);
+//         int right=climbStairs(n-2);
+
+//         return dp[n]=left+right;
+//     }
+
     public int climbStairs(int n) {
+        //Memoization
+//         int dp[]=new int [n+1];
+//         Arrays.fill(dp,-1);
+//         return f(n,dp);
 
-        // if(n==1) return 1;
-        // if(n==0) return 1;
-
-        // int left=climbStairs(n-1);
-        // int right=climbStairs(n-2);
-
-        // return left+right;
-
+        
+        //Tabulation
         if(n<=3){
             return n;
         }
