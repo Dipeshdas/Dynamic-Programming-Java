af23814e03d1a0b277993df3e6a1936f7b789651
Dipesh Das <77893779+Dipeshdas@users.noreply.github.com>
2022-12-21 11:36:10 +0530

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,38 +1,35 @@
-//Memoization not possible here .
 class Solution {
     
       int dp[]=new int[46];
 
-    //Memoization
-//     int f(int n,int dp[]){
-//         if(n==1) return 1;
-//         if(n==0) return 1;
-//         if(dp[n]!=-1) return dp[n];
-//         int left=climbStairs(n-1);
-//         int right=climbStairs(n-2);
+    int f(int n,int dp[]){
+        if(n==1) return 1;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int left=f(n-1,dp);
+        int right=f(n-2,dp);
 
-//         return dp[n]=left+right;
-//     }
+        return dp[n]=left+right;
+    }
 
     public int climbStairs(int n) {
-        //Memoization
-//         int dp[]=new int [n+1];
-//         Arrays.fill(dp,-1);
-//         return f(n,dp);
+        int dp[]=new int [n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
 
         
-        //Tabulation
-        if(n<=3){
-            return n;
-        }
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=3;
+
+        // if(n<=3){
+        //     return n;
+        // }
+        // dp[0]=0;
+        // dp[1]=1;
+        // dp[2]=2;
+        // dp[3]=3;
         
-        for(int i=4;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        // for(int i=4;i<=n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];
     }
 }
